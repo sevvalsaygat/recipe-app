c987a055ea2240dae24da4cf41217b19a63c14c8
Şevval Saygat <65735565+sevvalsaygat@users.noreply.github.com>
2024-03-05 17:55:33 +0300

    feat: create design new recipe page (#6)

--- components/Icons/index.ts
+++ components/Icons/index.ts
@@ -3,5 +3,7 @@ import Food from "./Food";
 import Recipe from "./Recipe";
 import Time from "./Time";
 import Cooking from "./Cooking";
+import Plus from "./Plus";
+import Cutlery from "./Cutlery";
 
-export default { Delete, Food, Recipe, Time, Cooking };
+export default { Delete, Food, Recipe, Time, Cooking, Plus, Cutlery };
--- containers/NewRecipe/Form/index.tsx
+++ containers/NewRecipe/Form/index.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation";
 import { useForm, useFieldArray, FormProvider } from "react-hook-form";
 
 import { api } from "@app/hooks";
-import { Button, Form as FormComponents } from "@app/components";
+import { Button, Form as FormComponents, Icons } from "@app/components";
 import { useRecipeStore } from "@app/stores";
 
 type FormPropTypes = {};
@@ -24,7 +24,7 @@ const Form: React.FC<FormPropTypes> = () => {
   });
 
   const { handleSubmit, reset, control, watch } = useFormMethods;
-  const { fields, append } = useFieldArray<IRecipeFormType>({
+  const { fields, append, remove } = useFieldArray<IRecipeFormType>({
     control,
     name: "materials",
   });
@@ -32,8 +32,6 @@ const Form: React.FC<FormPropTypes> = () => {
   const addRecipe = useRecipeStore((state) => state.addRecipe);
 
   const onSubmit = (data: IRecipeFormType) => {
-    console.log("On submit calıstı, formdan gelen data = ", data);
-
     try {
       addRecipe(data);
       reset();
@@ -50,37 +48,38 @@ const Form: React.FC<FormPropTypes> = () => {
 
   return (
     <div className="w-full flex flex-row">
-      <div className="flex flex-col items-center w-1/2">
+      <div className="flex flex-col items-center w-2/3">
         <FormProvider {...useFormMethods}>
-          <div className="flex flex-col px-11 py-8 border overflow-auto w-3/4 h-72 my-20">
+          <div className="food-bar-bg flex flex-col px-11 py-8 border overflow-auto w-3/4 h-80 my-20">
             {materialsData?.data.map((data, index) => {
               return (
                 <div
-                  className="flex flex-row justify-between cursor-pointer hover:bg-gray-200 pl-3 py-2"
+                  className="flex flex-row text-white font-serif text-base items-center justify-between cursor-pointer bg-gray-800 bg-opacity-25 hover:bg-lime-700 hover:bg-opacity-75 pl-7 py-2"
                   key={index}
                 >
                   <div>{data.name}</div>
-                  <Button
+                  <Icons.Plus
                     onClick={() => {
                       append({
                         name: data.name,
                         quantity: 1,
                       });
                     }}
-                    title="+"
-                    className="px-3 border bg-red-500"
+                    className="w-8 h-8 text-white mr-3 hover:text-gray-800"
                   />
                 </div>
               );
             })}
           </div>
           <div className="flex flex-col w-2/3">
-            <div className="flex flex-row gap-4 items-center justify-center">
+            <div className="flex flex-row gap-4 mb-7 items-center justify-center">
               <div>
                 <FormComponents.Input
                   variant="primary"
                   name="title"
                   label="Recipe Title"
+                  placeholder="(e.g. Spaghetti)"
+                  rules={{ required: "This field is required!" }}
                 />
               </div>
               <div>
@@ -88,6 +87,8 @@ const Form: React.FC<FormPropTypes> = () => {
                   variant="primary"
                   name="cookingTime"
                   label="Cooking Time"
+                  placeholder="(e.g. 30 minutes)"
+                  rules={{ required: "This field is required!" }}
                 />
               </div>
               <div>
@@ -95,6 +96,8 @@ const Form: React.FC<FormPropTypes> = () => {
                   variant="primary"
                   name="cookingMethod"
                   label="Cooking Method"
+                  placeholder="(e.g. Boil, Fry)"
+                  rules={{ required: "This field is required!" }}
                 />
               </div>
             </div>
@@ -102,22 +105,46 @@ const Form: React.FC<FormPropTypes> = () => {
               onClick={handleSubmit(onSubmit)}
               variant="primary"
               className="w-full"
-              title="save"
+              title="Save Recipe"
             />
           </div>
         </FormProvider>
       </div>
-      <div className="flex flex-row w-1/2 mx-28 mt-16 border">
-        <div className="flex flex-col px-20 mt-7">
-          <div className="mb-10">{watchTitle}</div>
-          <div className="flex flex-col h-52 overflow-x-auto pr-9 ">
-            {fields.map((field, index) => {
-              return <div key={index}>{field.name}</div>;
-            })}
+      <div className="flex flex-row w-1/3 mx-28 mt-16 bg-orange-50 rounded-2xl shadow-lg">
+        <div className="flex flex-col px-20 justify-between">
+          <div className="flex flex-col mt-7">
+            <div className="mb-10 pb-1 text-lg font-mono text-orange-900 underline decoration-double">
+              {watchTitle}
+            </div>
+            <div className="flex flex-col h-72 w-full overflow-x-auto pr-9">
+              {fields.map((field, index) => {
+                return (
+                  <div
+                    className="flex flex-row items-center justify-between w-full gap-6 text-sm font-mono text-orange-700"
+                    key={index}
+                  >
+                    <div className="flex flex-row items-center w-full ">
+                      <Icons.Cutlery />
+                      {field.name}
+                    </div>
+                    <Icons.Delete
+                      onClick={() => {
+                        remove(index);
+                      }}
+                      className="w-6 h-6 cursor-pointer text-red-700 hover:text-red-500 "
+                    />
+                  </div>
+                );
+              })}
+            </div>
           </div>
-          <div className="flex flex-col mt-10">
-            <div>{watchCookingTime}</div>
-            <div>{watchCookingMethod}</div>
+          <div className="flex flex-col mb-24">
+            <div className="text-sm font-serif text-orange-700 underline">
+              {watchCookingTime}
+            </div>
+            <div className="text-sm font-serif text-orange-700 underline">
+              {watchCookingMethod}
+            </div>
           </div>
         </div>
       </div>
